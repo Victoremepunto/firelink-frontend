244716065afdfce0fef2037b6bb588eb466bca02
Adam Drew <addrew@redhat.com>
2024-01-29 09:37:49 -0500

    Docker config

--- build_deploy.sh
+++ build_deploy.sh
@@ -14,14 +14,20 @@ fi
 # Define your image name
 IMAGE_NAME="firelink-frontend:$GIT_COMMIT_HASH"
 
+DOCKER_CONF="$PWD/.docker"
+mkdir -p "$DOCKER_CONF"
+
+docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
+docker --config="$DOCKER_CONF" login -u="$RH_REGISTRY_USER" -p="$RH_REGISTRY_TOKEN" registry.redhat.io
+
 # Build the Docker image
-docker build -t "$IMAGE_NAME" .
+docker --config="$DOCKER_CONF" build -t "$IMAGE_NAME" .
 
 # Tag the image for Quay
 QUAY_IMAGE="quay.io/cloudservices/$IMAGE_NAME"
-docker tag "$IMAGE_NAME" "$QUAY_IMAGE"
+docker --config="$DOCKER_CONF" tag "$IMAGE_NAME" "$QUAY_IMAGE"
 
 # Push the image to Quay
-docker push "$QUAY_IMAGE"
+docker --config="$DOCKER_CONF" push "$QUAY_IMAGE"
 
 echo "Image pushed to Quay: $QUAY_IMAGE"
