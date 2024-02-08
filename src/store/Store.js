df567c7ae03b943a43d7400fc93ba57beddbfdb7
Adam Drew <addrew@redhat.com>
2024-02-08 13:51:28 -0500

    Local storage backed persistence

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,8 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import appSliceReducer from './AppSlice'
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-export default configureStore({
-  reducer: {
-    appSlice: appSliceReducer,
-  },
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const rootReducer = combineReducers({ appSlice: appSliceReducer })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const Store = configureStore({
+  reducer: persistedReducer,
 })
+
+export const Persistor = persistStore(Store)
