235fb10b7d043f70c815360d4f8b4f8bb0c55195
Adam Drew <addrew@redhat.com>
2024-03-12 08:27:05 -0400

    Deploy modal timeouts, error handling, etc

--- src/deploy/AppDeployModal.js
+++ src/deploy/AppDeployModal.js
@@ -1,115 +1,161 @@
-import React from "react";
-import { useState} from "react";
+import React, { useEffect, useRef } from "react";
+import { useState } from "react";
 import io from "socket.io-client";
-import { 
-    Button, 
-    Modal, 
-    ModalVariant, 
-} from '@patternfly/react-core';
+import { Button, Modal, ModalVariant } from "@patternfly/react-core";
 import { Spinner } from "@patternfly/react-core";
-import CheckCircle from '@patternfly/react-icons/dist/js/icons/check-circle-icon';
-import TimesCircle from '@patternfly/react-icons/dist/js/icons/times-circle-icon';
-import InfoCircle from '@patternfly/react-icons/dist/js/icons/info-circle-icon';
+import CheckCircle from "@patternfly/react-icons/dist/js/icons/check-circle-icon";
+import TimesCircle from "@patternfly/react-icons/dist/js/icons/times-circle-icon";
+import InfoCircle from "@patternfly/react-icons/dist/js/icons/info-circle-icon";
 import { useSelector } from "react-redux";
-import {
-    getDeploymentOptions,
-} from "../store/AppDeploySlice";
+import { getDeploymentOptions } from "../store/AppDeploySlice";
 
-const DEPLOY_EVENT = 'deploy-app';
-const ERROR_EVENT = 'error-deploy-app';
-const MONITOR_EVENT = 'monitor-deploy-app';
-const END_EVENT = 'end-deploy-app';
+const DEPLOY_EVENT = "deploy-app";
+const ERROR_EVENT = "error-deploy-app";
+const MONITOR_EVENT = "monitor-deploy-app";
+const END_EVENT = "end-deploy-app";
+const TIMEOUT = 60000; // 60 seconds timeout
 
 // Construct the WebSocket URL based on the current location
-const protocol = window.location.protocol === 'https:' ? 'wss://' : 'ws://';
+const protocol = window.location.protocol === "https:" ? "wss://" : "ws://";
 const host = window.location.host;
 const SERVER = `${protocol}${host}`;
 
-export default function AppDeployModal({buttonLabel, disabled, buttonVariant}) {
-    
-    const deploymentOptions = useSelector(getDeploymentOptions);
-    const initialResponse = { message: "Initiating deployment connection..."};
+export default function AppDeployModal({
+  buttonLabel,
+  disabled,
+  buttonVariant,
+}) {
+  const deploymentOptions = useSelector(getDeploymentOptions);
+  const initialResponse = { message: "Initiating deployment connection..." };
 
-    const [showModal, setShowModal] = useState(false);
-    const [canCloseModal, setCanCloseModal] = useState(false);
-    const [wsResponses, setWsResponses] = useState([initialResponse]);
+  const [showModal, setShowModal] = useState(false);
+  const [canCloseModal, setCanCloseModal] = useState(false);
+  const [wsResponses, setWsResponses] = useState([initialResponse]);
+  let timeoutActive = useRef(false);
+  let socket = useRef(null);
 
-    const Deploy = () => {
-        setWsResponses([initialResponse]);
-        const socket = io(SERVER, { path: '/api/firelink/socket.io', transports: ['polling'] });
+  useEffect(() => {
+    return () => {
+      if (socket.current) {
+        socket.current.disconnect();
+      }
+    };
+  }, [socket]);
 
-        socket.on(MONITOR_EVENT, (response) => {
-            setWsResponses(state => [...state, response]);
-        });
-    
-        socket.on(ERROR_EVENT, (response) => {
-            setWsResponses(state => [...state, response]);
-        });
-    
-        socket.on(END_EVENT, (response) => {
-            setWsResponses(state => [...state, response]);
-            setCanCloseModal(true);
-            socket.disconnect();
-        });
-        console.log(deploymentOptions)
-        socket.emit(DEPLOY_EVENT, deploymentOptions);
-        setShowModal(true);
-    } 
+  const Deploy = () => {
+    setWsResponses([initialResponse]);
+    timeoutActive.current = true;
 
-    const StatusIcon = ({index, response}) => {
-        if (index === wsResponses.length - 1 && !canCloseModal)  {
-            return <Spinner  size="md"/>
-        }
-        if (response.error === true) {
-            return <TimesCircle color="#FF0000"/>
-        }
-        if ( response.completed === true) {
-            return <CheckCircle color="#00FF00"/>
-        }   
-        return <InfoCircle color="#00AAFF"/>
+    if (socket.current) {
+      socket.current.disconnect();
     }
 
-    const close = () => { setShowModal(false) }
+    socket.current = io(SERVER, {
+      path: "/api/firelink/socket.io",
+      transports: ["polling"],
+    });
 
-    const getButtonLabel = () => {
-        if (buttonLabel) {
-            return buttonLabel;
-        }
-        return "Deploy";
+    const timeout = setTimeout(() => {
+      if (timeoutActive.current) {
+        setWsResponses((state) => [
+          ...state,
+          {
+            message:
+              "Error: Server is unresponsive. Try again or contact DevProd.",
+          },
+        ]);
+        setCanCloseModal(true);
+        socket.current.disconnect();
+      }
+    }, TIMEOUT);
+
+    socket.current.on(MONITOR_EVENT, (response) => {
+      timeoutActive.current = false;
+      setWsResponses((state) => [...state, response]);
+    });
+
+    socket.current.on(ERROR_EVENT, (response) => {
+      timeoutActive.current = false;
+      setWsResponses((state) => [...state, response]);
+      setCanCloseModal(true);
+      socket.disconnect();
+    });
+
+    socket.current.on(END_EVENT, (response) => {
+      timeoutActive.current = false;
+      setWsResponses((state) => [...state, response]);
+      setCanCloseModal(true);
+      socket.current.disconnect();
+    });
+
+    socket.current.on("disconnect", () => {
+      clearTimeout(timeout); // Ensure the timeout is cleared when the socket disconnects
+    });
+
+    console.log(deploymentOptions);
+    socket.current.emit(DEPLOY_EVENT, deploymentOptions);
+    setShowModal(true);
+  };
+
+  const StatusIcon = ({ index, response }) => {
+    if (index === wsResponses.length - 1 && !canCloseModal) {
+      return <Spinner size="md" />;
     }
-
-    const getButtonVariant = () => {
-        if (buttonVariant) {
-            return buttonVariant;
-        }
-        return "primary";
+    if (response.error === true) {
+      return <TimesCircle color="#FF0000" />;
     }
+    if (response.completed === true) {
+      return <CheckCircle color="#00FF00" />;
+    }
+    return <InfoCircle color="#00AAFF" />;
+  };
 
-    return <React.Fragment> 
-        <Modal
+  const close = () => {
+    setShowModal(false);
+    timeoutActive.current = false; // Ensure the timeout is disabled when the modal is closed
+  };
+
+  const getButtonLabel = () => {
+    return buttonLabel || "Deploy";
+  };
+
+  const getButtonVariant = () => {
+    return buttonVariant || "primary";
+  };
+
+  return (
+    <React.Fragment>
+      <Modal
         variant={ModalVariant.small}
         title="Deploying..."
         isOpen={showModal}
         showClose={canCloseModal}
         onClose={close}
         actions={[
-            <Button key="cancel" onClick={close} >
-                Close
-            </Button>
-        ]}>
-            <div style={{height: '14rem', overflow: 'auto'}}>
-                <ul>
-                    {wsResponses.map((response, index) => {
-                        return <li key={`response-id-${index}`}>
-                                &nbsp; &nbsp; &nbsp; <StatusIcon index={index} response={response}/> &nbsp; {response.message}
-                            </li>})
-                    }
-                </ul>
-            </div>
-        </Modal>
-        <Button onClick={Deploy} isDisabled={disabled} variant={getButtonVariant()}>
-            {getButtonLabel()}
-        </Button>
+          <Button key="cancel" onClick={close}>
+            Close
+          </Button>,
+        ]}
+      >
+        <div style={{ height: "14rem", overflow: "auto" }}>
+          <ul>
+            {wsResponses.map((response, index) => (
+              <li key={`response-id-${index}`}>
+                &nbsp; &nbsp; &nbsp;{" "}
+                <StatusIcon index={index} response={response} /> &nbsp;{" "}
+                {response.message}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Modal>
+      <Button
+        onClick={Deploy}
+        isDisabled={disabled}
+        variant={getButtonVariant()}
+      >
+        {getButtonLabel()}
+      </Button>
     </React.Fragment>
-
+  );
 }
